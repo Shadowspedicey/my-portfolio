9eb1f3488c21a43e14badbc9af783c2f6259735d
Ahmed Khaled <shadowspedicey@gmail.com>
2025-10-05 01:40:50 +0300

    Added the white spotlight at the corner of the screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      
+			<div className="w-[50%] h-50 bg-white/5 blur-[75px] rounded-full absolute top-0 right-0 transform translate-x-1/6 -translate-y-1/3 md:w-[40%] lg:h-75"></div>
     </>
   )
 }
