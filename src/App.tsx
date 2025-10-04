7538221d367863f056db1dd4f1782080e97d1cbb
Ahmed Khaled <shadowspedicey@gmail.com>
2025-10-05 01:41:11 +0300

    Added the navbar (non-functioning)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Navbar from "./Navbar"
+
 function App() {
 
-  return (
-    <>
+	return (
+		<>
 			<div className="w-[50%] h-50 bg-white/5 blur-[75px] rounded-full absolute top-0 right-0 transform translate-x-1/6 -translate-y-1/3 md:w-[40%] lg:h-75"></div>
-    </>
-  )
+			<main className="md:w-1/2 m-auto">
+				<Navbar />
+			</main>
+		</>
+	)
 }
 
 export default App
