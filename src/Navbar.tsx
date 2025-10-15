ab73938b5323ce2ad3eaf4fe52fbace37f6cbc30
Ahmed Khaled <shadowspedicey@gmail.com>
2025-10-15 16:16:11 +0300

    Added smooth scrolling from navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useRef } from "react";
+
 export default function Navbar() {
+	const linksRef = useRef<HTMLUListElement | null>(null);
+
+	useEffect(() => {
+		if (!linksRef.current)
+			return;
+
+		function handleClick(e: MouseEvent, a: HTMLAnchorElement) {
+			e.preventDefault();
+			document.getElementById(a.hash.substring(1))?.scrollIntoView({ behavior: "smooth" });
+		};
+
+		const anchors = Array.from(linksRef.current.children).map(li => li.children[0] as HTMLAnchorElement);
+		anchors.forEach(a => a.addEventListener("click", e => handleClick(e, a)));
+
+		return () => anchors.forEach(a => a.removeEventListener("click", e => handleClick(e, a)));
+	}, [])
+
 	return(
 		<nav>
-			<ul className="flex justify-center md:justify-end p-5 gap-10 text-white font-bold text-sm">
+			<ul className="flex justify-center md:justify-end p-5 gap-10 text-white font-bold text-sm" ref={linksRef}>
 				<li><a href="#home">Home</a></li>
 				<li><a href="#about">About</a></li>
 				<li><a href="#projects">Projects</a></li>
