169ea334fe53ad3e2ab219d7e5608b312b3f3b5e
Ahmed Khaled <shadowspedicey@gmail.com>
2026-04-14 00:39:08 +0200

    Update the copyright year

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return(
 		<footer className="with-lighter-primary-background py-2">
 			<div className="md:scale-110"><Navbar centered /></div>
-			<div className="text-white text-center text-md font-kanit">&copy; 2025 Ahmed Khaled. All Rights Reserved.</div>
+			<div className="text-white text-center text-md font-kanit">&copy; 2026 Ahmed Khaled. All Rights Reserved.</div>
 		</footer>
 	);
 }
